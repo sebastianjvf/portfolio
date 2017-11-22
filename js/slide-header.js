0ccd5037cd5d6fe47e67dee96fdf861bc8da2942
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-22 12:09:15 +0100

    feat: Added slide header script.

---
+++ js/slide-header.js
@@ -0,0 +1,20 @@
+(function() {
+	
+	window.addEventListener('load', toggleHeader);
+	window.addEventListener('scroll', toggleHeader);
+	
+	function toggleHeader() {
+		
+		var header = document.getElementsByTagName('header')[0];
+		header.addEventListener('click', function() {
+			scrollIt(0);
+		});
+		
+		if (window.pageYOffset > header.offsetHeight*3) {
+			header.style.top = '0';
+		} else {
+			header.style.top = '-50%';
+		}
+	}
+	
+})();
