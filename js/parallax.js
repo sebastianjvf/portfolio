96a250464db0860c2a5cf19c28d9ce20f89dea34
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-12-06 19:55:43 +0100

    Added parallax file.

---
+++ js/parallax.js
@@ -0,0 +1,18 @@
+(function() {
+	
+	window.addEventListener('load', function(event) {
+		
+		let lines = document.getElementById('lines');
+		
+		window.addEventListener('scroll', function(event) {
+			
+			let scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+			
+			console.log('Scroll!');
+			lines.style.top = scrollPos * - 0.15 + 'px';
+			
+		});
+
+	});
+	
+})();
