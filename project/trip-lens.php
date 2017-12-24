75b882258f5431f366b40386030c399d4c82623c
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-12-24 03:01:44 +0100

    feat: Added pictures to the trip lens case study.

--- project/trip-lens.php
+++ project/trip-lens.php
@@ -4,10 +4,12 @@
 	$project_title = '- Trip Lens';
 
 	// Set the description
-	$project_description = '<h1>Trip Lens</h1>
+	$project_description = '
+	<span class="date">February 2017</span>
+	<h1>Trip Lens</h1>
 	
 	<p>
-	
+		Trip Lens is a design concept designed to facilitate travel planning in groups. Users can set up profiles by adding information such as adding their passport type, time restrictions and maximum distance to destination. Users can then create trips, which show a map and a lens. Attaching users from the trip, represented as bubbles, to the lens will show countries to which the attached users can and want to travel together.
 	</p>
 	';
 	
@@ -21,6 +23,116 @@
 	// Content
 ?>
 
+<section id="project-one" class="content">
+	<div class="inner">
+		<h1>Role</h1>
+		<h2>Tasks</h2>
+		<p>
+			Trip lens was implemented in a group of four design students. Tasks for each group member remained largely the same through the project, especially so during the ideation phase; group members executed interviews separately to ensure a large pool of insights and ideas. My tasks in later phases of the design cycle were creating the user flow and the paper prototype.
+		</p>
+		<h2>Methods and tools</h2>
+		<ul class="topics">
+			<li>Interviews</li>
+			<li>Personas</li>
+			<li>Scenarios</li>
+			<li>User flows</li>
+			<li>Paper prototyping</li>
+			<li>Video prototyping</li>
+		</ul>
+	</div>
+</section>
+
+<section id="project-two" class="content">
+	<div class="inner">
+		<h1>Ideation</h1>
+		<h2>Task</h2>
+		<p>
+			The task was to develop a design concept which solves a user problem in the domain of travel and/or public transport. The goal of the class was to go through the user-centred design cycle rapidly and deliver a final video prototype at the end of one week. As part of the ideation phase, group members went to observe users and conduct interviews. My interviews were conducted in a group travelling abroad.
+		</p>
+		
+		<h2>Interviews and analysis</h2>
+		<p>
+			The interviews conducted were quite informal; rather than guiding the conversation, I first observed how users were currently solving their problem and what tools they were using. Thereafter, I asked questions about what they thought caused problems during the planning phase.
+		</p>
+		
+		<p>
+			From my observations and interviews I could see that every participant in the travel group had their own tools for dealing with problems. Participants seemed to disagree about the tools, mentioning what made them the perfect fit; the problem was that other members were unfamiliar with the tools someone else usually used. Some participants were not sure about which countries they could go to, some did not like to go to certain countries which had been mentioned. A table on a paper was created for each country marking if the person would like to go there and would be able to go, according to the passport. As a last step, a common time frame had to be fun. After a small discussion, a Doodle was used with 90 days.
+		</p>
+		
+		<p>
+			Two other members turned out to have interviewed people planning trips abroad as well; we had thus collected useful insight and decided to focus our design concept on group travelling.
+		</p>
+		
+		<h2>Personas</h2>
+		<p>
+			As a first step, we analysed the participants from the group travels we had interviewed. As a result, we set up three personas, bundling intentions and problems according to the insights we had gathered:
+		</p>
+		
+		<ol>
+			<li>Ben, the traveller without time</li>
+			<li>Nicole, the traveller with a restrictive passport</li>
+			<li>Damien, the picky traveller</li>
+		</ol>
+		
+		<h2>Scenario</h2>
+		<p>
+			A scenario was written down, in which we modelled a common group situation; later on, we would rewrite the same scene into a story board to when users used our design concept for the video prototype. The main parts of the scene played out the three problems we had identified: time, place and passport restrictions.
+		</p>
+		
+		<h2>Design brief</h2>
+		<p>
+			The design concept of trip lens is centred around the lens to which users can attach their personal information. Moving the lens over a map will show if users, according to their personal information, can and want to go to a certain country by colouring the country accordingly.
+		</p>
+	</div>
+</section>
+
+<section id="project-three" class="content">
+	<div class="inner">
+		<h1>Iterative prototyping</h1>
+		<h2>User flows</h2>
+		<p>
+			To gather requirements that users have, we created a user flow based on our findings. The flows shows a single time line, which starts by adding the personal information. The flow then branches out when the user either joins or creates a trip. Finally, the user can explore the countries by moving the lens over the map.
+		</p>
+		
+		<h2>Wireframing</h2>
+		<p>
+			We created wireframes for the three parts of the user interface: Setting up a user profile, creating a trip and adding users and exploring the map and handling the lens.
+		</p>
+		
+		<h2>Paper prototyping</h2>
+		<p>
+			We created a paper prototype using transparent foil to simulate changing elements and highlighting parts of the map. We also realised that the metaphor of a lens could be expanded; as the lens itself constitutes an object itself, reifying a filter for the attached user information, we added a delete button to indicate that several lenses with different information could be created. Letting a user superpose lenses pushes the explorational nature of the concept even further.
+		</p>
+		
+		<div class="img-caption">
+			<img src="" alt="">
+			<span></span>
+		</div>
+		
+		<p>
+			
+		</p>
+		
+		<h2>User testing</h2>
+		<p>
+		</p>
+		
+		<h2>Video prototyping</h2>
+		<p>
+		</p>
+</section>
+
+<section id="project-four" class="content">
+	<div class="inner">
+		<h1>Evaluation</h1>
+		<h2>Cognitive walkthroughs</h2>
+		<p>
+		</p>
+		
+		<h1>Conclusion</h1>
+		<p>
+		</p>
+</section>
 
 <?php 
 
